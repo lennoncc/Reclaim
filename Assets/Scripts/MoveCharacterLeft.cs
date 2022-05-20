e57f6ee40d3cd37e615251fc6d3f7f6e0283b129
lennoncc <lennon.c.cruz@gmail.com>
2022-05-19 23:26:21 -0700

    Utilized CaptainController code, created sidescrolling scene

---
+++ Assets/Scripts/MoveCharacterLeft.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Captain.Command;
+
+namespace Captain.Command
+{
+    public class MoveCharacterLeft : ScriptableObject, ICaptainCommand
+    {
+        private float speed = 5.0f;
+
+        public void Execute(GameObject gameObject)
+        {
+            var rigidBody = gameObject.GetComponent<Rigidbody2D>();
+            if (rigidBody != null)
+            {
+                if(rigidBody.velocity.x > -5)
+                {
+                    rigidBody.velocity = new Vector2(-this.speed, rigidBody.velocity.y);
+                }
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MoveCharacterRight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Captain.Command;
+
+namespace Captain.Command
+{
+    public class MoveCharacterRight : ScriptableObject, ICaptainCommand
+    {
+        private float speed = 5.0f;
+
+        public void Execute(GameObject gameObject)
+        {
+            var rigidBody = gameObject.GetComponent<Rigidbody2D>();
+            if (rigidBody != null)
+            {
+                // right movement would have a positive vector
+                if(rigidBody.velocity.x < 5)
+                {
+                    rigidBody.velocity = new Vector2(this.speed, rigidBody.velocity.y);
+                }
+                // the sprite isn't flipped when moving right
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+        }
+    }
+}
