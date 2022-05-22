2000094ec2e2cb7b0293a2adba7aa543fe21ab64
Judy Zhang <judy.j.zhang@gmail.com>
2022-05-22 13:38:06 -0700

    Forest background added for level 1, also added parallax script for background and camera follow script

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+    
+    private float length;
+    private float startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start ()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update() 
+    {
+        float dist = (cam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+    }
+}
