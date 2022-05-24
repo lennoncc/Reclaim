6918910ef8b73ad9d6e193af65a7663fa44a4bde
mattom12 <72368245+mattom12@users.noreply.github.com>
2022-05-23 23:47:08 -0700

    Renamed movement files, added jump mechanic. Working on adding momentum and responsiveness. - Matt

---
+++ Assets/Scripts/JumpHero.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Hero.Command;
+
+namespace Hero.Command
+{
+    // Phase 4 Implementation: Fire2 set to Jump
+    // Added private bool variable in PirateController.cs called "isStateJump" to track status of jump
+    // Purpose is to prevent multiple jumps from occuring midair
+    // Can only jump while touching the ground
+    // Note: Added a "Ground" tag that was applied to the three Ground objects in MushroomForest, Mountains, and Graveyard
+    public class JumpHero : ScriptableObject, IHeroCommand
+    {
+        private float force = 5.0f;
+
+
+        public void Execute(GameObject gameObject)
+        {
+            var rigidBody = gameObject.GetComponent<Rigidbody2D>();
+            if (rigidBody != null)
+            {
+                // This applies a force in the upwards direction to simulate a jump
+                rigidBody.velocity = Vector2.up * force;
+            }
+        }
+    }
+}
