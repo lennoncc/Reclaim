cc3a80bb3d60e0a0f8f41c4bb943ebc282b5c8c3
lennoncc <lennon.c.cruz@gmail.com>
2022-05-31 23:25:36 -0700

    broke more stuff, fixed it again

--- Assets/Scripts/InGameDialogueTrigger.cs
+++ Assets/Scripts/InGameDialogueTrigger.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueTrigger : MonoBehaviour
+public class InGameDialogueTrigger : MonoBehaviour
 {
     public Dialogue[] dialogue;
 
     public void TriggerDialogue ()
     {
-        // FindObjectOfType<PanCamera>().enabled = true;
+        FindObjectOfType<PanCamera>().enabled = true;
         FindObjectOfType<DialogueManager>().EnqueueDialogues(dialogue);
         // FindObjectOfType<PanCamera>().enabled = true;
     }
@@ -19,9 +19,9 @@ public class DialogueTrigger : MonoBehaviour
             // Debug.Log(other.tag);
             FindObjectOfType<DialogueManager>().EnqueueDialogues(dialogue);
             // other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-            // other.GetComponent<CamberMovement>().animator.SetFloat("Velocity", 0);
-            // other.GetComponent<CamberMovement>().enabled = false;
-            // FindObjectOfType<PanCamera>().enabled = true;
+            other.GetComponent<CamberMovement>().animator.SetFloat("Velocity", 0);
+            other.GetComponent<CamberMovement>().enabled = false;
+            FindObjectOfType<PanCamera>().enabled = true;
 
             // GameObject.Find("Camber").GetComponent<CamberMovement>().enabled = false;
         }
